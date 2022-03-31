3ef8ea4ca574972000c876bbaa8fd6be0b1f4c90
Mayur Darade <mayurdarade99@gmail.com>
2022-03-31 17:47:42 +0530

    day40task

---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+
+if (fs.existsSync("date-time.html")) {
+  fs.unlinkSync("date-time.html");
+}
+
+fs.writeFileSync(
+  "date-time.html",
+  "<head><title>Date-time task</title></head>"
+);
+
+setInterval(() => {
+  fs.appendFile(
+    "date-time.html",
+    `
+    <p>${new Date().toLocaleTimeString()}</p>`,
+    "utf-8",
+    (err) => {
+      if (err) throw err;
+      console.log("data stored");
+    }
+  );
+}, 60000);
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send(fs.readFileSync("date-time.html", "utf8"));
+});
+
+app.listen(PORT, () => console.log("listening on port", PORT));
